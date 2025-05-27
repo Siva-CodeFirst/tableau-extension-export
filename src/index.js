e04ede338eab2a3aa828a6af2ef69ed7fc9c13ee
Siva-CodeFirst <urssivakumar@outlook.com>
2025-05-26 20:07:05 -0400

    created min file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import Main from './containers/Main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(Main, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
